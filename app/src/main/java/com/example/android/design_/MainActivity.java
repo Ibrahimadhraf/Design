90dbabacc99237e5d9cc0fb5382aa61bd5b1570b
Ibrahimadhraf <33137985+Ibrahimadhraf@users.noreply.github.com>
2019-04-26 19:38:50 +0200

    add

--- app/src/main/java/com/example/android/design_/MainActivity.java
+++ app/src/main/java/com/example/android/design_/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.android.design_;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.balysv.materialripple.MaterialRippleLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+ MaterialRippleLayout register_mrl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register_mrl=findViewById(R.id.register_mrl);
+        register_mrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/design_/RegisterActivity.java
+++ app/src/main/java/com/example/android/design_/RegisterActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.design_;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+         setSupportActionBar(toolbar);
+         getSupportActionBar().setDisplayHomeAsUpEnabled( true);
+         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
+         getSupportActionBar().setTitle("");
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+              onBackPressed();
+              return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
